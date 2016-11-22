ea869c84f9fbaf6904e2e6ef6e4ced6b50c0bd15
Robb Schiefer <rschiefer@live.com>
2016-11-22 13:33:57 -0600

    Add project files.

---
+++ src/SeleniumInDotNetCoreTest/On_google_search_page.cs
@@ -0,0 +1,40 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SeleniumInDotNetCoreTest
+{
+    public class On_google_search_page : IDisposable
+    {
+        private IWebDriver _driver;
+
+        public On_google_search_page()
+        {
+            var capabilities = DesiredCapabilities.InternetExplorer();
+            _driver = new RemoteWebDriver(new Uri("http://localhost:5555"), capabilities);
+        }
+
+        public void Dispose()
+        {
+        }
+
+        [Fact]
+        public void Should_find_search_box()
+        {
+            _driver.Navigate().GoToUrl("http://www.google.com/ncr");
+            Console.WriteLine(_driver.Title);
+
+            IWebElement query = _driver.FindElement(By.Name("q"));
+            query.SendKeys("TestingBot");
+            query.Submit();
+            Console.WriteLine(_driver.Title);
+
+            _driver.Quit();
+
+        }
+    }
+}
